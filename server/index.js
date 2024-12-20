6bfb9e205011597c6991cda7d5029dfa3bdc9042
Harsh <harsh.verma1503@gmail.com>
2024-12-21 03:18:26 +0530

    hosting

--- server/index.js
+++ server/index.js
@@ -7,6 +7,11 @@ const authRoutes = require('./routes/auth.js');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = process.env.TWILIO_MESSAGING_SERVICE_SID;
+const twilioClient = require('twilio')(accountSid, authToken);
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
